e1ac1abc68d1ef767f91747bf38bcbc89de3750e
Bumskee <fadhlan.razan@binus.ac.id>
2022-06-03 20:01:05 +0700

    make the project ready for vercel deployment

--- Django/mysite/settings.py
+++ Django/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')+fhp5@@6e^dwo9n$msq6rv&&%q#_t(6*ouhh6q8+k*+lv-a2$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'now.sh']
 
 
 # Application definition
@@ -128,5 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
